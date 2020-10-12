900430dde329c86dc43b3db0bd76558b4576b89b
woodchuckchoi <choiggoggi@gmail.com>
2020-10-12 18:02:49 +0900

    research init

--- research/udp.c
+++ research/udp.c
@@ -12,14 +12,31 @@ int main() {
     int serv_sock;
     struct sockaddr_in serv_addr = {0,};
     int serv_addrlen = sizeof(serv_addr);
+    char buff[1024] = {0, };
 
-    serv_sock = socket(AF_INET, SOCK_DGRAM);
+    serv_sock = socket(AF_INET, SOCK_DGRAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(7777);
 
     bind(serv_sock, (struct sockaddr *)&serv_addr, (socklen_t)serv_addrlen);
     listen(serv_sock, 100);
+
+    int cli_sock;
+    struct sockaddr_in cli_addr = {0,};
+    int cli_addrlen = sizeof(cli_addr);
+    int val;
+
+	while (1) {
+		if ((cli_sock = accept(serv_sock, (struct sockaddr *)&cli_addr, (socklen_t *)&cli_addrlen)) > 0) {
+            val = read(cli_sock, buff, 1024);
+            if (val > 0) {
+                printf("Read\n\n%s\n", buff);
+            }
+            close(cli_sock);
+        }
+    }
+    close(serv_sock);
     
     return 0;
 }
